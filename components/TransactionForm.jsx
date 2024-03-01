57fd018a44e7f0fcd398199de0c7076380b770f6
Haiyah Perez <haiyahperez@pursuit.org>
2024-03-01 15:28:23 -0500

    added links to transactions

--- components/TransactionForm.jsx
+++ components/TransactionForm.jsx
@@ -1,6 +1,11 @@
 import { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
-const TransactionForm = ({ setTransaction, setToggleForm, edit, setEdit }) => {
+
+
+const TransactionForm = ({ setTransactions, setToggleForm, edit, setEdit }) => {
+    const navigate = useNavigate;
+    const { id } = useParams; 
     const [transaction, setTransaction] = useState({
         item: "",
         amount: "",
@@ -8,18 +13,40 @@ const TransactionForm = ({ setTransaction, setToggleForm, edit, setEdit }) => {
         from: "",
         category: ""
     })
-}
 
-function handleChange(e) {
-    setTransaction({ ...transaction, [e.target.id]: e.target.value });
-}
 
-function handleSubmit() {
+    function handleChange(e) {
+        setTransaction({ ...transaction, [e.target.id]: e.target.value });
+    }
 
-}
-function handleSubmit() {
+    function handleSubmit(e) {
+        e.preventDefault();
+    
+        const options = {
+          method: id ? "PUT" : "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(transaction),
+        };
+    
+        const url = id
+          ? `http://localhost:3003/transactions/${id}`
+          : "http://localhost:3003/transactions";
+    
+        fetch(url, options)
+          .then((res) => res.json())
+          .then((data) => {
+            setTransactions(data.transactions);
+            setToggleForm(false);
+            navigate("/");
+          })
+          .catch((error) => {
+            console.error("Error:", error);
+          });
+      }
 
-}
+    function handleCancel(){
+        navigate("/")
+} 
 
 return (
     <div>
@@ -80,5 +107,5 @@ return (
         <button onClick={handleCancel}>Cancel</button>
     </div>
 )
-
-export default TransactionForm; 
+}
+export default TransactionForm;
--- components/Transactions.jsx
+++ components/Transactions.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from "react-router-dom" 
 
 const Transactions = ({ transactions, setTransactions }) => {
   if (transactions.length === 0) return null;
@@ -7,9 +8,17 @@ const Transactions = ({ transactions, setTransactions }) => {
     <div>
       {transactions.map(({ id, item_name, amount, date }) => (
         <div key={id}>
-          <h3>Item Name:{item_name}</h3>
-          <p>Amount:{amount}</p>
-          <p>date:{date}</p>
+            <h3>Item Name:{item_name}</h3>
+                <p>Amount:{amount}</p>
+                <p>date:{date}</p>
+            <div>
+                <Link to={`/${ id }`}>
+                    <button>details</button>
+                </Link>
+                <Link to={`/edit/${ id }`}>
+                    <button>edit</button>
+                </Link>
+            </div>
         </div>
       ))}
     </div>
